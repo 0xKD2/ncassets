59ba6de82cb4257d7bb88bb37ab238b36c10d707
Wararit Satitnimankan <manesz13@gmail.com>
2023-04-10 17:23:01 +0700

    kd-script-v.1.0.1.js

---
+++ kd-script-v.1.0.1.js
@@ -0,0 +1,7 @@
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+      {/* elementor */}
+      postPElement = document.querySelectorAll(".elementor-widget-theme-post-content p");
+      postPElement[10].appendChild(document.createElement('div')).innerHTML='<a href="https://pgzeed365.com/" style="display: block; text-align: center; margin-bottom: 30px;"><img src="http://via.placeholder.com/640x360" style="margin: auto;" /></a>';
+    });
+</script>
