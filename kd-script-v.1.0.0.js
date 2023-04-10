1b3fd25a7cd4a969a6e1d29f28e43cd2a0584d9d
Wararit Satitnimankan <manesz13@gmail.com>
2023-04-10 17:03:35 +0700

    Update kd-script-v.1.0.0.js

--- kd-script-v.1.0.0.js
+++ kd-script-v.1.0.0.js
@@ -1,9 +1,11 @@
 <script>
-    {/* theme magazine */}
-    postP = document.querySelectorAll(".entry-content p");
-    postP[10].appendChild(document.createElement('div')).innerHTML='<a href="https://pgzeed365.com/" style="display: block; text-align: center; margin-bottom: 30px;"><img src="http://via.placeholder.com/640x360" style="margin: auto;" /></a>';
+    document.addEventListener("DOMContentLoaded", () => {
+      {/* theme magazine */}
+      postP = document.querySelectorAll(".entry-content p");
+      postP[10].appendChild(document.createElement('div')).innerHTML='<a href="https://pgzeed365.com/" style="display: block; text-align: center; margin-bottom: 30px;"><img src="http://via.placeholder.com/640x360" style="margin: auto;" /></a>';
 
-    {/* elementor */}
-    postPElement = document.querySelectorAll(".elementor-widget-theme-post-content p");
-    postPElement[10].appendChild(document.createElement('div')).innerHTML='<a href="https://pgzeed365.com/" style="display: block; text-align: center; margin-bottom: 30px;"><img src="http://via.placeholder.com/640x360" style="margin: auto;" /></a>';
+      {/* elementor */}
+      postPElement = document.querySelectorAll(".elementor-widget-theme-post-content p");
+      postPElement[10].appendChild(document.createElement('div')).innerHTML='<a href="https://pgzeed365.com/" style="display: block; text-align: center; margin-bottom: 30px;"><img src="http://via.placeholder.com/640x360" style="margin: auto;" /></a>';
+    });
 </script>
